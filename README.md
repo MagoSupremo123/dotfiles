b2a0b210613ede076b68bccb46f23be12f93195e
MagoSupremo123 <gabrielbranco07@yahoo.com>
2026-03-09 22:14:08 -0300

    Basic stuff

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # dotfiles
+
+## MPV
+
+%APPDATA%\mpv\input.conf
+
+%APPDATA%\mpv\mpv.conf
+
+## Powershell
+
+%USERPROFILE%\Documents\Powershell\profile.ps1
+
+List of modules is a txt file
---
+++ powershell/profile.ps1
@@ -0,0 +1,27 @@
+#Imports
+Import-Module posh-git
+Import-Module Terminal-Icons
+Import-Module Microsoft.WinGet.CommandNotFound
+
+#PSReadLine
+Set-PSReadLineOption -EditMode Windows
+Set-PSReadLineOption -PredictionSource HistoryAndPlugin
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+
+#Fzf
+Import-Module PSFzf
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
+
+# Utilities
+function which ($command) {
+  Get-Command -Name $command -ErrorAction SilentlyContinue |
+    Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+}
+#Aliases
+Set-Alias ll ls
+Set-Alias g git
+
+#Oh My Posh
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/takuya.omp.json" | Invoke-Expression
